814bbbaaa0b39124132802572c1cda3350993553
likun <likun@joynova.com.cn>
2020-09-10 15:53:28 +0800

    optimize

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gogo/protobuf/jsonpb"
 	"joynova.com/joynova/joymicro/examples/hello/handler"
 	"joynova.com/joynova/joymicro/examples/hello/proto"
 	"joynova.com/joynova/joymicro/service"
 )
 
-
 func main() {
+	//p := &proto.Request{
+	//	User: &proto.User{
+	//		Name: "lk",
+	//	},
+	//}
+	//m := jsonpb.Marshaler{EmitDefaults: true}
+	//str, err := m.MarshalToString(p)
+	//fmt.Printf("%v, %v\n", str, err)
+	//return
+
 	s, err := service.New(":8888", []string{"192.168.1.188:2332"})
-		if err != nil {
+	if err != nil {
 		panic(err)
 	}
 	err = proto.RegisterHelloHandler(s, new(handler.HelloHandler))
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ replace google.golang.org/grpc => google.golang.org/grpc v1.29.0
 
 require (
 	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/gogo/protobuf v1.3.1
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/websocket v1.4.1 // indirect
